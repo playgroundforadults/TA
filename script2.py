895e960c4e0fdba610f93cd08a502ca67ac900f9
playgroundforadults <nabilsjarief@gmail.com>
2025-09-21 19:43:22 +0700

    second commit

---
+++ script2.py
@@ -0,0 +1,7 @@
+number_1= int(input("Enter a number: "))
+number_2 = int(input("Enter a number: "))
+operation = input("Enter a operation: ")
+if operation == "*":
+    print(number_1 * number_2)
+elif operation == "/":
+    print(number_1 / number_2)
