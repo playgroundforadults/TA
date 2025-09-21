9d936c864276976af28db036afb794709b0e1ea6
playgroundforadults <nabilsjarief@gmail.com>
2025-09-21 19:36:52 +0700

    first commit

---
+++ script.py
@@ -0,0 +1,7 @@
+number_1 = input("Enter a number: ")
+number_2 = input("Enter a number: ")
+operation = input("Enter a operation: ")
+if operation == "+":
+    print(number_1 + number_2)
+if operation == "-":
+    print(number_1 - number_2)
